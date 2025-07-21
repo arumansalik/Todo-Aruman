0b27c2efca5f658a6ddfc77dc4a51f11c069d33a
unknown <arumansalik45@gmail.com>
2025-07-21 23:29:57 +0530

    First Commit

---
+++ client/script.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://admin:KJIBcmmiPhuqeLlC@cluster0.4gpl4ag.mongodb.net/todo-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
