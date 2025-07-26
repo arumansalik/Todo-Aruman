a8f834d72f8d6e84f385005a5d447123b2500784
unknown <arumansalik45@gmail.com>
2025-07-27 00:46:47 +0530

    SignUp on Client Side

--- client/script.js
+++ client/script.js
@@ -1,1 +1,20 @@
-const
+const API = "https://localhost:3000";
+
+function signup() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const name = document.getElementById("name").value;
+
+    axios.post(`${API}/signup`, {
+        name,
+        email,
+        password
+    })
+    .then(res => {
+        alert("Signed Up!");
+    })
+    .catch(err => {
+        console.log(err);
+        alert("Signup failed");
+    });
+}
