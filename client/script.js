625d892ea1e4f372cac250851d977378e0b45798
unknown <arumansalik45@gmail.com>
2025-07-27 00:39:56 +0530

    FrontEnd Added

--- client/script.js
+++ client/script.js
@@ -1,6 +1,1 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://admin:KJIBcmmiPhuqeLlC@cluster0.4gpl4ag.mongodb.net/todo-db", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+const
